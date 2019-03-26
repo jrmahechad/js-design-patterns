91297288f2a31179dc9ced98df9fbc7c101b9919
Julian Mahecha <jmahecha@H5740.ad.hugeinc.com>
2019-03-26 14:14:21 -0500

    abstract factory

---
+++ creational/abstract_factory.js
@@ -0,0 +1,95 @@
+'use strict';
+
+class AbstractFactory {
+    constructor() {
+    }
+
+    createProductA (product) {
+    }
+
+    createProductB (product) {
+    }
+}
+
+//Each concrete factory extends from AbstractFactory and implement it's methods
+class ConcreteFactory1 extends AbstractFactory {
+    constructor() {
+        super()
+        console.log("ConcreteFactory1 class created");
+    }
+
+    createProductA (product) {
+        console.log('ConcreteFactory1 createProductA')
+        return new ProductA1()
+    }
+
+    createProductB (product) {
+        console.log('ConcreteFactory1 createProductB')
+        return new ProductB1()
+    }
+}
+
+class ConcreteFactory2 extends AbstractFactory {
+    constructor() {
+        super()
+        console.log("ConcreteFactory2 class created");
+    }
+
+    createProductA (product) {
+        console.log('ConcreteFactory2 createProductA')
+        return new ProductA2()
+    }
+
+    createProductB (product) {
+        console.log('ConcreteFactory2 createProductB')
+        return new ProductB2()
+    }
+}
+
+class AbstractProductA {
+    constructor() {
+    }
+}
+
+class AbstractProductB {
+    constructor() {
+    }
+}
+
+
+class ProductA1 extends AbstractProductA {
+    constructor() {
+        super()
+        console.log('ProductA1 created')
+    }
+}
+
+class ProductA2 extends AbstractProductA {
+    constructor() {
+        super()
+        console.log('ProductA2 created')
+    }
+}
+
+class ProductB1 extends AbstractProductB {
+    constructor() {
+        super()
+        console.log('ProductB1 created')
+    }
+}
+
+class ProductB2 extends AbstractProductB {
+    constructor() {
+        super()
+        console.log('ProductB2 created')
+    }
+}
+
+function init_AbstractFactory() {
+    var factory1 = new ConcreteFactory1()
+    var productB1 = factory1.createProductB()
+    
+    var factory2 = new ConcreteFactory2()
+    var productA2 = factory2.createProductA()
+}
+init_AbstractFactory();
---
+++ readme.md
@@ -0,0 +1,3 @@
+This project presents some design patterns in ES6 (EcmaScript 6)
+
+Based on https://loredanacirstea.github.io/es6-design-patterns/
